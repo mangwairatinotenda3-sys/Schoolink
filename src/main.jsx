935d4bd4f85e46ffac720b3a6a3a39151886ced7
mangwairatinotenda3-sys <mangwairatinotenda3@gmail.com>
2026-07-30 17:37:21 +0200

    Wrap App component with PresenceProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { PresenceProvider } from './lib/presence.js'
 import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <AuthProvider>
-        <App />
+        <PresenceProvider>
+          <App />
+        </PresenceProvider>
       </AuthProvider>
     </HashRouter>
   </React.StrictMode>
@@ -18,4 +22,4 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register(import.meta.env.BASE_URL + 'sw.js')
   })
-      }
+    }
