65b7a0ad420bafed4a80b29627b50571fad48595
mangwairatinotenda3-sys <mangwairatinotenda3@gmail.com>
2026-09-08 14:43:55 +0200

    Refactor routes and imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ import ChatThread from './pages/ChatThread.jsx'
 import Communities from './pages/Communities.jsx'
 import CreateCommunity from './pages/CreateCommunity.jsx'
 import CommunityDetail from './pages/CommunityDetail.jsx'
+import CommunityGroupInfo from './pages/CommunityGroupInfo.jsx'
+import JoinCommunityByInvite from './pages/JoinCommunityByInvite.jsx'
 import BursarDashboard from './pages/BursarDashboard.jsx'
 import CoachDashboard from './pages/CoachDashboard.jsx'
 import ICTDashboard from './pages/ICTDashboard.jsx'
@@ -39,6 +41,13 @@ import BlockedUsers from './pages/BlockedUsers.jsx'
 import PostDetail from './pages/PostDetail.jsx'
 import MyPosts from './pages/MyPosts.jsx'
 import Connections from './pages/Connections.jsx'
+import Polls from './pages/Polls.jsx'
+import SelectAlumniSchool from './pages/SelectAlumniSchool.jsx'
+import AlumniDirectory from './pages/AlumniDirectory.jsx'
+import Achievements from './pages/Achievements.jsx'
+import CounsellingDashboard from './pages/CounsellingDashboard.jsx'
+import ReceptionistDashboard from './pages/ReceptionistDashboard.jsx'
+import MoreHub from './pages/MoreHub.jsx'
 import Home from './pages/Home.jsx'
 import Profile from './pages/Profile.jsx'
 import Settings from './pages/Settings.jsx'
@@ -46,14 +55,6 @@ import Saved from './pages/Saved.jsx'
 import Notifications from './pages/Notifications.jsx'
 import AddPost from './pages/AddPost.jsx'
 import Dashboard from './pages/Dashboard.jsx'
-import CommunityGroupInfo from './pages/CommunityGroupInfo.jsx'
-import JoinCommunityByInvite from './pages/JoinCommunityByInvite.jsx'
-import Polls from './pages/Polls.jsx'
-import SelectAlumniSchool from './pages/SelectAlumniSchool.jsx'
-import AlumniDirectory from './pages/AlumniDirectory.jsx'
-import Achievements from './pages/Achievements.jsx'
-import CounsellingDashboard from './pages/CounsellingDashboard.jsx'
-import ReceptionistDashboard from './pages/ReceptionistDashboard.jsx'
 
 function RequireAuth({ children }) {
   const { user, loading } = useAuth()
@@ -82,6 +83,7 @@ export default function App() {
         <Route path="/onboarding/create-school" element={<RequireAuth><CreateSchool /></RequireAuth>} />
         <Route path="/onboarding/join-school" element={<RequireAuth><JoinSchool /></RequireAuth>} />
         <Route path="/onboarding/join-student" element={<RequireAuth><JoinAsStudent /></RequireAuth>} />
+        <Route path="/onboarding/select-alumni-school" element={<RequireAuth><SelectAlumniSchool /></RequireAuth>} />
         <Route path="/invite-member" element={<RequireAuth><InviteMember /></RequireAuth>} />
         <Route path="/staff-directory" element={<RequireAuth><StaffDirectory /></RequireAuth>} />
         <Route path="/pending-approvals" element={<RequireAuth><PendingApprovals /></RequireAuth>} />
@@ -92,6 +94,8 @@ export default function App() {
         <Route path="/chats/:userId" element={<RequireAuth><ChatThread /></RequireAuth>} />
         <Route path="/communities" element={<RequireAuth><Communities /></RequireAuth>} />
         <Route path="/communities/create" element={<RequireAuth><CreateCommunity /></RequireAuth>} />
+        <Route path="/communities/join/:inviteCode" element={<RequireAuth><JoinCommunityByInvite /></RequireAuth>} />
+        <Route path="/communities/:communityId/info" element={<RequireAuth><CommunityGroupInfo /></RequireAuth>} />
         <Route path="/communities/:communityId" element={<RequireAuth><CommunityDetail /></RequireAuth>} />
         <Route path="/bursar" element={<RequireAuth><BursarDashboard /></RequireAuth>} />
         <Route path="/coach" element={<RequireAuth><CoachDashboard /></RequireAuth>} />
@@ -113,8 +117,12 @@ export default function App() {
         <Route path="/post/:postId" element={<RequireAuth><PostDetail /></RequireAuth>} />
         <Route path="/my-posts" element={<RequireAuth><MyPosts /></RequireAuth>} />
         <Route path="/connections/:type" element={<RequireAuth><Connections /></RequireAuth>} />
+        <Route path="/polls" element={<RequireAuth><Polls /></RequireAuth>} />
+        <Route path="/alumni" element={<RequireAuth><AlumniDirectory /></RequireAuth>} />
+        <Route path="/achievements" element={<RequireAuth><Achievements /></RequireAuth>} />
         <Route path="/counselling" element={<RequireAuth><CounsellingDashboard /></RequireAuth>} />
         <Route path="/reception" element={<RequireAuth><ReceptionistDashboard /></RequireAuth>} />
+        <Route path="/more" element={<RequireAuth><MoreHub /></RequireAuth>} />
 
         <Route path="/home" element={<RequireAuth><Home /></RequireAuth>} />
         <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
@@ -123,15 +131,9 @@ export default function App() {
         <Route path="/notifications" element={<RequireAuth><Notifications /></RequireAuth>} />
         <Route path="/add-post" element={<RequireAuth><AddPost /></RequireAuth>} />
         <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
-        <Route path="/communities/join/:inviteCode" element={<RequireAuth><JoinCommunityByInvite /></RequireAuth>} />
-        <Route path="/communities/:communityId/info" element={<RequireAuth><CommunityGroupInfo /></RequireAuth>} />
-        <Route path="/polls" element={<RequireAuth><Polls /></RequireAuth>} />
-        <Route path="/onboarding/select-alumni-school" element={<RequireAuth><SelectAlumniSchool /></RequireAuth>} />
-        <Route path="/alumni" element={<RequireAuth><AlumniDirectory /></RequireAuth>} />
-        <Route path="/achievements" element={<RequireAuth><Achievements /></RequireAuth>} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   )
-  }
+        }
